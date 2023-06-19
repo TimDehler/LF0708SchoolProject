ac753b25e4706699d2aff5513edf92b89588606a
Tim <tim.dehler@tibidi.de>
2023-06-19 20:26:09 +0200

    data mapping with dom manipulation

--- Website/script.js
+++ Website/script.js
@@ -7,15 +7,33 @@ async function getData() {
   const temp = await myData.json();
 
   let ulList = document.createElement("ul");
+  ulList.setAttribute("class", "list");
 
   temp.map((cust) => {
     let liElement = document.createElement("li");
-    //liElement.appendChild(document.createTextNode(JSON.stringify(cust)));
-    liElement.appendChild(document.createTextNode("Name: " + cust.Name + " "));
-    liElement.appendChild(document.createTextNode("Age: " + cust.Age));
+    let span = liElement.appendChild(document.createElement("span"));
+    span.setAttribute("class", "id");
+    span.innerHTML = cust._id;
+    let div = liElement.appendChild(document.createElement("div"));
+    div.setAttribute("class", "details");
+    let name_span = div.appendChild(document.createElement("span"));
+    name_span.setAttribute("class", "name");
+    name_span.innerHTML = cust.Name;
+    let age_span = div.appendChild(document.createElement("span"));
+    name_span.setAttribute("class", "age");
+    age_span.innerHTML = cust.Age;
+
     ulList.appendChild(liElement);
   });
   myDoc.appendChild(ulList);
+
+  const listItems = document.querySelectorAll(".list li");
+
+  listItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active");
+    });
+  });
 }
 
 getData();
