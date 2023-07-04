c53fd866cc2151021527fb6ee8c48505e76aafc5
Tim Dehler <tim_dehler@goodyear.com>
2023-07-04 08:16:42 +0200

    final test

--- Website/IFrame/iframe.mjs
+++ Website/IFrame/iframe.mjs
@@ -3,18 +3,24 @@ import { mapObject } from "../utils.mjs";
 const container = document.getElementById("container");
 const mqtt_url = "http://localhost:3000/mqtt-data";
 
-let previousData = null;
+let oldData;
 
-async function getMqttData() {
-  await fetch(mqtt_url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((e) => {
-      console.log(`Purposly caught ${e}`);
-    });
-}
+const getMqttData = async () => {
+  let fetchedData = await fetch(mqtt_url);
+  let freshData;
+  try {
+    freshData = await fetchedData.json();
+  } catch (e) {
+    if (document.getElementById("status") === null) {
+      console.log(`Caught: ${e}`);
+      const status_message = document.createElement("h1");
+      status_message.setAttribute("id", "status");
+      status_message.textContent = "There is no new data to display!";
+      container.appendChild(status_message);
+    }
+  }
+  return freshData;
+};
 
 const addEvent = () => {
   const listItems = document.querySelectorAll(".list li");
@@ -26,25 +32,25 @@ const addEvent = () => {
   });
 };
 
-const appendNewMQTTData = () => {
-  previousData = data;
-  container.innerHTML = "";
-  container.appendChild(mapObject(data));
-  addEvent();
-};
-
 setInterval(async () => {
+  const listItem = document.getElementById("list-item");
   const data = await getMqttData();
-
-  if (data === undefined) return;
-
-  if (previousData === null) {
-    appendNewMQTTData();
+  if (listItem === null) {
+    container.appendChild(mapObject(data));
+    addEvent();
   }
 
-  if (previousData === data) {
-    return;
-  } else {
-    appendNewMQTTData();
+  if (
+    listItem !== null &&
+    oldData._id === undefined &&
+    data._id === undefined
+  ) {
+    const status = document.getElementById("status");
+    if (status) {
+      status.remove();
+    }
+    listItem.remove();
+    container.appendChild(mapObject(data));
+    addEvent();
   }
 }, 5000);
